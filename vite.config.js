caa31dac1d1b55998e7293c392227c42934ca764
Lucia Morua <lu.morua22@gmail.com>
2026-04-16 21:36:52 -0300

    Agregar sitemap.xml para SEO

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sitemap from 'vite-plugin-sitemap'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://blog-escritores.web.app',
+      routes: [
+        '/',
+        '/blog',
+        '/allBlogs',
+        '/nosotros',
+        '/contacto',
+      ]
+    })
+  ],
   define: {
     global: 'globalThis',
   },
